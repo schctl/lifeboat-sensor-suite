1b70266ed1c6d4427fa554c41a4c334ed21fc4be
Bhoomi Thakwani <78319241+bhoomithakwani@users.noreply.github.com>
2024-10-04 00:36:50 +0530

    Add files via upload

---
+++ mlx90614.ino
@@ -0,0 +1,34 @@
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  if (!mlx.begin()) {
+    Serial.println("Error connecting to MLX sensor. Check wiring.");
+    while (1);
+  };
+}
+
+void loop() {
+  Serial.print("Ambient temperature = "); 
+  Serial.print(mlx.readAmbientTempC());
+  Serial.print("°C");      
+  Serial.print("   ");
+  Serial.print("Object temperature = "); 
+  Serial.print(mlx.readObjectTempC()); 
+  Serial.println("°C");
+  
+  Serial.print("Ambient temperature = ");
+  Serial.print(mlx.readAmbientTempF());
+  Serial.print("°F");      
+  Serial.print("   ");
+  Serial.print("Object temperature = "); 
+  Serial.print(mlx.readObjectTempF()); 
+  Serial.println("°F");
+
+  Serial.println("-----------------------------------------------------------------");
+  delay(1000);
+}
---
+++ pulse_sensor.ino
@@ -0,0 +1,58 @@
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+
+#define REPORTING_PERIOD_MS     1000
+#define INTERRUPT_PIN 6 // Change this to the pin connected to the INT pin of the MAX30100
+
+PulseOximeter pox;
+
+// Interrupt service routine
+void ICACHE_RAM_ATTR handleInterrupt() {
+    // Handle the interrupt by reading data from MAX30100
+    pox.update();
+}
+
+
+// Time at which the last beat occurred
+uint32_t tsLastReport = 0;
+
+// Callback routine is executed when a pulse is detected
+void onBeatDetected() {
+    Serial.println("♥ Beat!");
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    Serial.print("Initializing pulse oximeter..");
+
+    // Initialize sensor
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+    }
+
+  // Configure sensor to use 7.6mA for LED drive
+  pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+
+    // Register a callback routine
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+}
+
+void loop() {
+    // Read from the sensor
+    pox.update();
+
+    // Grab the updated heart rate and SpO2 levels
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.print("Heart rate:");
+        Serial.print(pox.getHeartRate());
+        Serial.print("bpm / SpO2:");
+        Serial.print(pox.getSpO2());
+        Serial.println("%");
+
+        tsLastReport = millis();
+    }
+}
